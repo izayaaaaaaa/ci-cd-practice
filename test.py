658aa1060539b681e238fa8256ce374469c84e6a
Francyn Macadangdang <izaya.orihara0011@gmail.com>
2024-03-11 18:57:36 +0800

    updated dockerhub username secret

--- test.py
+++ test.py
@@ -12,4 +12,4 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello Izaya retest!" in response.data
+    assert b"Hello izayaaaaa!" in response.data
